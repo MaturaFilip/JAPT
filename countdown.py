3ede6f3ea887329057dddb19fd0c7980d1a2e921
MaturaFilip <matura.filip96@gmail.com>
2025-03-14 15:15:16 +0100

    app loop added

--- countdown.py
+++ countdown.py
@@ -4,11 +4,7 @@ import sys
 from playsound import playsound
 
 
-
-
-
 def count_down(m=25, s=0):
-
     total_seconds = m * 60 + s
 
     while total_seconds >= 0:
@@ -18,8 +14,34 @@ def count_down(m=25, s=0):
     
     return True
 
-def ring(m, s):
-    if count_down(m, s):
-        print("hey!")
+
+def app_loop():
+    print("Welcome to the JAPT - Just Another Pomodoro Timer!")
+    focus_min = int(input("How long should the focus session be? (minutes): "))
+    focus_sec = int(input("Sec: "))
+    rest = int(input("How long should the short break be? (minutes): "))
+    cycles = int(input("How many Pomodoro cycles? (default 4): "))
+
+    while True:
+        print("Starting Pomodoro timer! Time to work!")
+        for _ in range(0, cycles):
+            count_down(focus_min, focus_sec)
+            print("Time to rest!")
+            playsound('sounds/mixkit-arcade-retro-game-over-213.wav')
+
+            count_down(rest)
+            print("Time to work!")
+            playsound('sounds/mixkit-arcade-retro-game-over-213.wav')
+
+        print("Time to long break!")
+        count_down(m=15)
         playsound('sounds/mixkit-arcade-retro-game-over-213.wav')
+
+        next_round = input("Wanna start again? (Y/n)")
+
+        if next_round == "y" or next_round == "Y" or next_round == "":
+            continue
+        else:
+            print("Goodbye!")
+            break
 
--- main.py
+++ main.py
@@ -6,16 +6,7 @@ import countdown
 
 
 def main():
-    print("Welcome to the JAPT - Just Another Pomodoro Timer!")
-    focus_min = int(input("How long should the focus session be? (minutes): "))
-    focus_sec = int(input("Sec: "))
-    rest = int(input("How long should the short break be? (minutes): "))
-    cycles = int(input("How many Pomodoro cycles? (default 4): "))
-
-
-    print("Starting Pomodoro timer")
-    countdown.ring(focus_min, focus_sec)
-
+    countdown.app_loop()
 
 if __name__ == "__main__":
     main()
